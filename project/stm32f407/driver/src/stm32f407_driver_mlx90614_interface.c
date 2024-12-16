68456bc8f783ab4b39913924c7c765ff325cca9b
LibDriver <704839379@qq.com>
2024-12-16 21:10:01 +0800

    fix: fix doc errors

--- project/stm32f407/driver/src/stm32f407_driver_mlx90614_interface.c
+++ project/stm32f407/driver/src/stm32f407_driver_mlx90614_interface.c
@@ -66,10 +66,10 @@ uint8_t mlx90614_interface_iic_deinit(void)
 
 /**
  * @brief      interface iic bus read
- * @param[in]  addr is the iic device write address
- * @param[in]  reg is the iic register address
- * @param[out] *buf points to a data buffer
- * @param[in]  len is the length of the data buffer
+ * @param[in]  addr iic device write address
+ * @param[in]  reg iic register address
+ * @param[out] *buf pointer to a data buffer
+ * @param[in]  len length of the data buffer
  * @return     status code
  *             - 0 success
  *             - 1 read failed
@@ -82,10 +82,10 @@ uint8_t mlx90614_interface_iic_read(uint8_t addr, uint8_t reg, uint8_t *buf, uin
 
 /**
  * @brief     interface iic bus write
- * @param[in] addr is the iic device write address
- * @param[in] reg is the iic register address
- * @param[in] *buf points to a data buffer
- * @param[in] len is the length of the data buffer
+ * @param[in] addr iic device write address
+ * @param[in] reg iic register address
+ * @param[in] *buf pointer to a data buffer
+ * @param[in] len length of the data buffer
  * @return    status code
  *            - 0 success
  *            - 1 write failed
@@ -98,7 +98,7 @@ uint8_t mlx90614_interface_iic_write(uint8_t addr, uint8_t reg, uint8_t *buf, ui
 
 /**
  * @brief     interface scl write
- * @param[in] value is the input value
+ * @param[in] value input value
  * @return    status code
  *            - 0 success
  *            - 1 scl write failed
@@ -111,7 +111,7 @@ uint8_t mlx90614_interface_scl_write(uint8_t value)
 
 /**
  * @brief     interface sda write
- * @param[in] value is the input value
+ * @param[in] value input value
  * @return    status code
  *            - 0 success
  *            - 1 sda write failed
@@ -124,7 +124,7 @@ uint8_t mlx90614_interface_sda_write(uint8_t value)
 
 /**
  * @brief     interface delay ms
- * @param[in] ms
+ * @param[in] ms time
  * @note      none
  */
 void mlx90614_interface_delay_ms(uint32_t ms)
@@ -134,7 +134,7 @@ void mlx90614_interface_delay_ms(uint32_t ms)
 
 /**
  * @brief     interface print format data
- * @param[in] fmt is the format data
+ * @param[in] fmt format data
  * @note      none
  */
 void mlx90614_interface_debug_print(const char *const fmt, ...)
